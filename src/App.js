0e46a7145bbbd4cbb6ddcbdd7e5f60935e18d5cd
Hưng Phan <phandinhhungvp2001@gmail.com>
2023-04-05 09:27:12 +0700

    import template and create component
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+import ReactDOM from "react-dom";
+import Slide from './components/Slide';
+import Student from './components/Student';
+import Class from './components/Class';
+import Library from './components/Library';
+import Footer from './components/Footer';
+const App =()=>{
+  return(
+<>
+<Slide></Slide>
+<Student></Student>
+<Class></Class>
+<Library></Library>
+<Footer></Footer>
+</>
+  )
+
 }
 
 export default App;
